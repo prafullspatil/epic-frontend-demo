3963392ec605eb2bb12dc07c6c74f549c707dc53
prafullspatil08 <prafull.patil@mindbowser.com>
2025-09-18 14:01:48 +0530

    chore: remove FHIR client and Epic configuration settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,5 @@
 export const environment = {
-    production: false,
-    epicClientId: '1ad3a1a0-0d18-4ea4-9b0f-36b7f85f2f4a',
-    epicRedirectUri: 'https://b4290512fc22.ngrok-free.app/redirect',
-    epicFhirBaseUrl: 'https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4',
-    epicAuthUrl: 'https://fhir.epic.com/interconnect-fhir-oauth/oauth2/authorize',
-    epicTokenUrl: 'https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token',
-    epicScope: 'patient/*.* user/*.* patient/*.write user/*.write Observation.read Condition.read MedicationRequest.read  patient/DocumentReference.read patient/DocumentReference.write patient/DocumentReference.cruds   launch openid fhirUser online_access offline_access'
-  };
-  
+  production: false,
+  name: 'local',
+  ENDPOINT_URL: 'http://localhost:3000',
+};
